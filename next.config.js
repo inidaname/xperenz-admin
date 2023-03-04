c3a111ae49b526959e78e70a9678295e8997f1b6
Hassan Sani <saniyhassan@gmail.com>
2023-03-04 18:24:41 +0100

    setting up nextjs and file structure

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+  env: getEnvConfig(),
+}
+
+function getEnvConfig() {
+  const environment = process.env.TARGET_ENV || process.env.NODE_ENV;
+  try {
+    return require(`./env-${environment}.json`);
+  } catch (err) {
+    return require("./env-development.json");
+  }
+}
+
+module.exports = nextConfig
